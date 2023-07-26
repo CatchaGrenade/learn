1f14e7e414c1c5210a5e9a42b96af5055c0cc2aa
csq <2820546657@qq.com>
2023-07-27 00:45:21 +0800

    测试

--- src/functionalInterface/MyFunctionalInterfaceImpl.java
+++ src/functionalInterface/MyFunctionalInterfaceImpl.java
@@ -3,6 +3,6 @@ package functionalInterface;
 public class MyFunctionalInterfaceImpl implements MyFunctionalInterface{
     @Override
     public void method() {
-        System.out.println("使用继承重写接口方法");
+        System.out.println("使用继承重写接口方法。");
     }
 }
