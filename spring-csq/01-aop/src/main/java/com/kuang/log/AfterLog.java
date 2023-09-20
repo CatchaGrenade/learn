fc2a25808bf513c22330e1cb17591f113c0ad41e
csq <2820546657@qq.com>
2023-09-20 22:54:01 +0800

    1

---
+++ spring-csq/01-aop/src/main/java/com/kuang/log/AfterLog.java
@@ -0,0 +1,12 @@
+package com.kuang.log;
+
+import org.springframework.aop.AfterAdvice;
+import org.springframework.aop.AfterReturningAdvice;
+
+import java.lang.reflect.Method;
+
+public class AfterLog implements AfterReturningAdvice {
+    public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
+        System.out.println(target.getClass().getName()+"的"+method.getName()+"被执行了, 返回结果为"+returnValue);
+    }
+}
---
+++ spring-csq/01-aop/src/main/java/com/kuang/service/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.kuang.service;
+
+public class UserServiceImpl implements UserService{
+    public void add() {
+        System.out.println("add");
+    }
+
+    public void del() {
+        System.out.println("del");
+    }
+
+    public void upd() {
+        System.out.println("upd");
+    }
+
+    public void sel() {
+        System.out.println("sel");
+    }
+}
